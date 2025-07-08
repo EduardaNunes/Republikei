ae4f2475c49a49ef269b6a5196a5dccfa06b7fc8
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2025-07-08 10:42:28 -0300

    Cria componente menu

---
+++ src/components/menu/index.tsx
@@ -0,0 +1,15 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import { styles } from "./styles";
+import BottomContainer from "../bottomContainer";
+
+// precisa adicionar as rotas do menu depois
+export default function Menu() {
+  return (
+    <BottomContainer style={styles.container}>
+        <MaterialIcons name="search" size={32} color={"#fff"} />
+        <MaterialIcons name="favorite" size={32} color={"#fff"} />
+        <MaterialIcons name="map" size={32} color={"#fff"} />
+        <MaterialIcons name="person" size={32} color={"#fff"} />
+    </BottomContainer>
+  );
+}
