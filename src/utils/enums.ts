d28e5d25a7a75d6609972fa7a91807f8e84834cf
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2025-07-15 22:08:23 -0300

    Cria arquivos de enums

---
+++ src/utils/enums.ts
@@ -0,0 +1,42 @@
+type enums = {
+  id: string;
+  name: string;
+};
+
+export const characteristics: enums[] = [
+  { id: "1", name: "Aceita Animais"},
+  { id: "2", name: "Aceita Fumantes"},
+  { id: "3", name: "Com Piscina"},
+  { id: "4", name: "Com Quintal"},
+  { id: "5", name: "Luz Inclusa"},
+  { id: "6", name: "IPTU Incluso"},
+  { id: "7", name: "Condomínio Incluso"},
+  { id: "8", name: "Água Inclusa"},
+  { id: "9", name: "Gás Incluso"},
+  { id: "10", name: "Intenet Inclusa"},
+  { id: "11", name: "Serviço de Limpeza Incluso"},
+];
+
+export const vacancyType: enums[] = [
+  { id: "1", name: "Masculina"},
+  { id: "2", name: "Feminina"},
+  { id: "3", name: "Mista"},
+];
+
+export const furniture: enums[] = [
+  { id: "1", name: "Cama"},
+  { id: "2", name: "Fogão"},
+  { id: "3", name: "Geladeira"},
+  { id: "4", name: "Armário"},
+  { id: "5", name: "Máquina de Lavar Roupa"},
+  { id: "6", name: "Máquina de Lavar Louça"},
+  { id: "7", name: "Guarda Roupa"},
+  { id: "8", name: "Microondas"},
+  { id: "9", name: "Varal"},
+  { id: "10", name: "Mesa de Estudos"},
+  { id: "11", name: "Air Fryer"},
+  { id: "12", name: "Mesa de Jantar"},
+  { id: "13", name: "Ar Condicionado"},
+  { id: "14", name: "Colchão"},
+  { id: "15", name: "Televisão"},
+];
