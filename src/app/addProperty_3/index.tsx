71cb6bcdd2da21bd8171f83cbd84209da70dbfa9
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2025-07-21 10:45:08 -0300

    Atualiza páginas implementando o SelectableBlock

--- src/app/addProperty_3/index.tsx
+++ src/app/addProperty_3/index.tsx
@@ -1,59 +1,36 @@
-import { Image, View, ScrollView, Alert } from "react-native";
+import { View } from "react-native";
 
 import { styles } from "../../components/styles/addProperty";
 import SquareButton from "@/components/button";
-import Input from "@/components/input";
 import AppText from "@/components/appText";
 import Menu from "@/components/menu";
-import { Category } from "@/components/category";
-
-
+import SelectableBlock from "@/components/selectableBlock";
 
 export default function AddProperty_3() {
-
   return (
     <>
-    <View style={styles.container}>
-        <View  style={styles.titleContainer}>
-            <AppText style={styles.title}>CARACTERÍSTICAS</AppText>
-            <AppText style={styles.subtitle}>CARACTERÍSTICAS</AppText>
+      <View style={styles.container}>
+        <View style={styles.titleContainer}>
+          <AppText style={styles.title}>CARACTERÍSTICAS</AppText>
         </View>
         <View style={styles.geralContainer}>
-            <View  style={styles.inputContainer}>
-                <View style={styles.subCategoryContainer}>
-                    <Category name="Permite animais"></Category>
-                    <Category name="Permite fumantes"></Category>
-                    <Category name="Com Quintal"></Category>
-                    <Category name="Com Piscina"></Category>
-                    <Category name="Luz Inclusa"></Category>
-                    <Category name="Água Inclusa"></Category>
-                    <Category name="Condomínio Incluso"></Category>
-                    <Category name="Internet Inclusa"></Category>
-                </View>
-                <AppText style={styles.subtitle}>TIPO DE VAGA</AppText>
-                <View style={styles.subCategoryContainer}>
-                    <Category name="Feminina"></Category>
-                    <Category name="Masculina"></Category>
-                    <Category name="Mista"></Category>    
-                </View>
-                <AppText style={styles.subtitle}>TIPO DE MORADIA</AppText>
-                <View style={styles.subCategoryContainer}>
-                    <Category name="Compartilhada"></Category>
-                    <Category name="Completa"></Category>
-                </View>
-                <AppText style={styles.subtitle}>MOBILIADO?</AppText>
-                <View style={styles.subCategoryContainer}>
-                    <Category name="Sim"></Category>
-                    <Category name="Não"></Category>
-                </View>
-            </View>
-            <View  style={styles.buttonsContainer}>
-                <SquareButton name="Voltar" variant="mediumS"></SquareButton>
-                <SquareButton name="Continuar" variant="mediumP"></SquareButton>
-            </View>
+          <View style={styles.inputContainer}>
+            <AppText style={styles.subtitle}>CARACTERÍSTICAS</AppText>
+            <SelectableBlock type="characteristics"></SelectableBlock>
+            <AppText style={styles.subtitle}>TIPO DE VAGA</AppText>
+            <SelectableBlock type="vacancyType"></SelectableBlock>
+            <AppText style={styles.subtitle}>TIPO DE MORADIA</AppText>
+            <SelectableBlock type="housingType"></SelectableBlock>
+            <AppText style={styles.subtitle}>MOBILIADO?</AppText>
+            <SelectableBlock type="question"></SelectableBlock>
+          </View>
+          <View style={styles.buttonsContainer}>
+            <SquareButton name="Voltar" variant="mediumS"></SquareButton>
+            <SquareButton name="Continuar" variant="mediumP"></SquareButton>
+          </View>
         </View>
-    </View>
-    <Menu></Menu>
+      </View>
+      <Menu></Menu>
     </>
   );
 }
--- src/app/addProperty_5/index.tsx
+++ src/app/addProperty_5/index.tsx
@@ -1,60 +1,46 @@
-import { Image, View, ScrollView, Alert } from "react-native";
+import { View } from "react-native";
 
 import { styles } from "../../components/styles/addProperty";
 import SquareButton from "@/components/button";
 import Input from "@/components/input";
 import AppText from "@/components/appText";
 import Menu from "@/components/menu";
-import { Category } from "@/components/category";
-
-
+import SelectableBlock from "@/components/selectableBlock";
 
 export default function AddProperty_5() {
-
   return (
     <>
-    <View style={styles.container}>
-        <View  style={styles.titleContainer}>
-            <AppText style={styles.title}>COMPARTILHADA</AppText>
-            <AppText style={styles.subtitle}>SELECIONAR MORADIA</AppText>
+      <View style={styles.container}>
+        <View style={styles.titleContainer}>
+          <AppText style={styles.title}>COMPARTILHADA</AppText>
         </View>
         <View style={styles.geralContainer}>
-            <View  style={styles.inputContainer}>
-                <View style={styles.subCategoryContainer}>
-                    <Category name="República"></Category>
-                    <Category name="Quarto"></Category>
-                </View>
-                <AppText style={styles.subtitle}>QUANTIDADE DE PESSOAS</AppText>
-                <View style={styles.subinputContainer}>
-                    <Input variant="secondary" title="No Quarto" containerStyle={{ width: "48%" }} ></Input>
-                    <Input variant="secondary" title="Na Moradia" containerStyle={{ width: "48%" }} ></Input>
-                </View>
-                <AppText style={styles.subtitle}>SELECIONAR MÓVEIS</AppText>
-                <View style={styles.subCategoryContainer}>
-                    <Category name="Cama"></Category>
-                    <Category name="Colchão"></Category>
-                    <Category name="Mesa Jantar"></Category>
-                    <Category name="Varal"></Category>
-                    <Category name="Geladeira"></Category>
-                    <Category name="Guarda Roupa"></Category>
-                    <Category name="AirFryer"></Category>
-                    <Category name="Armário"></Category>
-                    <Category name="Escrivaninha"></Category>
-                    <Category name="Microondas"></Category>
-                    <Category name="Ar Condicionado"></Category>
-                    <Category name="Fogão"></Category>
-                    <Category name="Televisão"></Category>
-                    <Category name="Máquina de Lavar Roupa Roupa"></Category>
-                    <Category name="Máquina de Lavar Louça"></Category>
-                </View>
+          <View style={styles.inputContainer}>
+            <AppText style={styles.subtitle}>SELECIONAR MORADIA</AppText>
+            <SelectableBlock type="sharedHouseType"></SelectableBlock>
+            <AppText style={styles.subtitle}>QUANTIDADE DE PESSOAS</AppText>
+            <View style={styles.subinputContainer}>
+              <Input
+                variant="secondary"
+                title="No Quarto"
+                containerStyle={{ width: "48%" }}
+              ></Input>
+              <Input
+                variant="secondary"
+                title="Na Moradia"
+                containerStyle={{ width: "48%" }}
+              ></Input>
             </View>
-            <View  style={styles.buttonsContainer}>
-                <SquareButton name="Voltar" variant="mediumS"></SquareButton>
-                <SquareButton name="Continuar" variant="mediumP"></SquareButton>
-            </View>
+            <AppText style={styles.subtitle}>SELECIONAR MÓVEIS</AppText>
+            <SelectableBlock type="furniture"></SelectableBlock>
+          </View>
+          <View style={styles.buttonsContainer}>
+            <SquareButton name="Voltar" variant="mediumS"></SquareButton>
+            <SquareButton name="Continuar" variant="mediumP"></SquareButton>
+          </View>
         </View>
-    </View>
-    <Menu></Menu>
+      </View>
+      <Menu></Menu>
     </>
   );
 }
