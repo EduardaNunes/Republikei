b82c80e050a53a5a1cb746a55880f9b781d53401
Vanessa Trajano <vanessapalmeirakelmertrajano@gmail.com>
2025-07-22 11:49:22 -0300

    Atualiza ids dos enums

--- src/utils/enums.ts
+++ src/utils/enums.ts
@@ -4,65 +4,65 @@ type enums = {
 };
 
 export const characteristics: enums[] = [
-  { id: "characteristics-1", name: "Aceita Animais" },
-  { id: "characteristics-2", name: "Aceita Fumantes" },
-  { id: "characteristics-3", name: "Com Piscina" },
-  { id: "characteristics-4", name: "Com Quintal" },
-  { id: "characteristics-5", name: "Luz Inclusa" },
-  { id: "characteristics-6", name: "IPTU Incluso" },
-  { id: "characteristics-7", name: "Condomínio Incluso" },
-  { id: "characteristics-8", name: "Água Inclusa" },
-  { id: "characteristics-9", name: "Gás Incluso" },
-  { id: "characteristics-10", name: "Intenet Inclusa" },
-  { id: "characteristics-11", name: "Serviço de Limpeza Incluso" },
+  { id: "characteristics-animais", name: "Aceita Animais" },
+  { id: "characteristics-fumantes", name: "Aceita Fumantes" },
+  { id: "characteristics-piscina", name: "Com Piscina" },
+  { id: "characteristics-quintal", name: "Com Quintal" },
+  { id: "characteristics-luz", name: "Luz Inclusa" },
+  { id: "characteristics-iptu", name: "IPTU Incluso" },
+  { id: "characteristics-condominio", name: "Condomínio Incluso" },
+  { id: "characteristics-agua", name: "Água Inclusa" },
+  { id: "characteristics-gas", name: "Gás Incluso" },
+  { id: "characteristics-internet", name: "Intenet Inclusa" },
+  { id: "characteristics-limpeza", name: "Serviço de Limpeza Incluso" },
 ];
 
 export const vacancyType: enums[] = [
-  { id: "vacancyType-1", name: "Masculina" },
-  { id: "vacancyType-2", name: "Feminina" },
-  { id: "vacancyType-3", name: "Mista" },
+  { id: "vacancyType-masc", name: "Masculina" },
+  { id: "vacancyType-fem", name: "Feminina" },
+  { id: "vacancyType-mista", name: "Mista" },
 ];
 
 export const furniture: enums[] = [
-  { id: "furniture-1", name: "Cama" },
-  { id: "furniture-2", name: "Fogão" },
-  { id: "furniture-3", name: "Geladeira" },
-  { id: "furniture-4", name: "Armário" },
-  { id: "furniture-5", name: "Máquina de Lavar Roupa" },
-  { id: "furniture-6", name: "Guarda Roupa" },
-  { id: "furniture-7", name: "Microondas" },
-  { id: "furniture-8", name: "Máquina de Lavar Louça" },
-  { id: "furniture-9", name: "Varal" },
-  { id: "furniture-10", name: "Mesa de Estudos" },
-  { id: "furniture-11", name: "Air Fryer" },
-  { id: "furniture-12", name: "Mesa de Jantar" },
-  { id: "furniture-13", name: "Ar Condicionado" },
-  { id: "furniture-14", name: "Colchão" },
-  { id: "furniture-15", name: "Televisão" },
+  { id: "furniture-cama", name: "Cama" },
+  { id: "furniture-fogao", name: "Fogão" },
+  { id: "furniture-geladeira", name: "Geladeira" },
+  { id: "furniture-armario", name: "Armário" },
+  { id: "furniture-lavar_roupa", name: "Máquina de Lavar Roupa" },
+  { id: "furniture-guarda_roupa", name: "Guarda Roupa" },
+  { id: "furniture-microondas", name: "Microondas" },
+  { id: "furniture-lavar_louca", name: "Máquina de Lavar Louça" },
+  { id: "furniture-varal", name: "Varal" },
+  { id: "furniture-mesa_estudos", name: "Mesa de Estudos" },
+  { id: "furniture-air_fryer", name: "Air Fryer" },
+  { id: "furniture-mesa_jantar", name: "Mesa de Jantar" },
+  { id: "furniture-ar_condicionado", name: "Ar Condicionado" },
+  { id: "furniture-colchao", name: "Colchão" },
+  { id: "furniture-tv", name: "Televisão" },
 ];
 
 export const housingType: enums[] = [
-  { id: "housingType-1", name: "Compartilhada" },
-  { id: "housingType-2", name: "Completa" },
+  { id: "housingType-compartilhada", name: "Compartilhada" },
+  { id: "housingType-completa", name: "Completa" },
 ];
 
 export const completeHouseType: enums[] = [
-  { id: "completeHouseType-1", name: "Apartamento" },
-  { id: "completeHouseType-2", name: "Kitnet" },
-  { id: "completeHouseType-3", name: "Casa" },
+  { id: "completeHouseType-ape", name: "Apartamento" },
+  { id: "completeHouseType-kitnet", name: "Kitnet" },
+  { id: "completeHouseType-casa", name: "Casa" },
 ];
 
 export const sharedHouseType: enums[] = [
-  { id: "sharedHouseType-1", name: "República" },
-  { id: "sharedHouseType-2", name: "Quarto" },
+  { id: "sharedHouseType-republica", name: "República" },
+  { id: "sharedHouseType-quarto", name: "Quarto" },
 ];
 
 export const question: enums[] = [
-  { id: "question-1", name: "Sim" },
-  { id: "question-2", name: "Não" },
+  { id: "question-sim", name: "Sim" },
+  { id: "question-nao", name: "Não" },
 ];
 
 export const ranking: enums[] = [
-  { id: "ranking-1", name: "Preço min" },
-  { id: "ranking-2", name: "Preço max" },
+  { id: "ranking-min", name: "Preço min" },
+  { id: "ranking-max", name: "Preço max" },
 ];
