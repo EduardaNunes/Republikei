5e3dbaa179225eabd740c198c3fe1db44c4f8291
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2025-08-04 05:15:02 -0300

    Permite fazer logout + puxa informações de nome, login e telefone do supabase na página de profile do locador

--- src/app/profileLandLord/index.tsx
+++ src/app/profileLandLord/index.tsx
@@ -1,19 +1,51 @@
-import React, { useState } from "react";
-import { ScrollView, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Alert, ScrollView, View } from "react-native";
 import { styles } from "../../components/styles/profileRenter";
 import SquareButton from "@/components/button";
 import Input from "@/components/input";
 import AppText from "@/components/appText";
 import Menu from "@/components/menu";
+import { useRouter } from "expo-router";
+import { supabase } from "@/lib/supabase";
 
 export default function ProfileLandLord() {
+  const [loading, setLoading] = useState(true);
   const [isEditing, setIsEditing] = useState(false);
-  const [name, setName] = useState("NOME PERFIL");
+
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
   const [confirmarSenha, setConfirmarSenha] = useState("");
   const [celular, setCelular] = useState("");
   const [descricao, setDescricao] = useState("");
+
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    const { error } = await supabase.auth.signOut();
+
+    if (error) {
+      Alert.alert("Erro no Logout", error.message);
+    } else {
+      router.replace('/');
+    }
+  };
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      setLoading(true);
+      const { data: { user } } = await supabase.auth.getUser();
+
+      if (user) {
+        setName(user.user_metadata.display_name || 'Nome não definido');
+        setEmail(user.email || 'E-mail não encontrado');
+        setCelular(user.phone || 'Número não encontrado')
+      }
+      setLoading(false);
+    };
+
+    fetchUserData();
+  }, []);
 
   const handleSave = () => {
     if (senha !== confirmarSenha) {
@@ -60,7 +92,7 @@ export default function ProfileLandLord() {
             </>
           ) : (
             <>
-              <SquareButton name="Logout" variant="shortS" onPress={() => console.log("logout")} />
+              <SquareButton name="Logout" variant="shortS" onPress={handleLogout} />
               <SquareButton name="Editar" variant="shortP" onPress={() => setIsEditing(true)} />
             </>
           )}
