c81f38678b0665838951c8ebb2a3a7fda147cb4d
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2025-08-06 18:48:05 -0300

    Arruma standart para standard

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -36,7 +36,7 @@ export default function Menu() {
         <MaterialIcons name="search" size={28} color="#fff" />
       </TouchableOpacity>
 
-      {userType == "standart" ? (
+      {userType == "standard" ? (
         <TouchableOpacity onPress={() => router.push("/favorites")}>
           <MaterialIcons name="favorite" size={28} color="#fff" />
         </TouchableOpacity>
