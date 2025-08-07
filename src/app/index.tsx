765aab4d52a4d705de50c2eeb7e071e05a7494e6
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2025-08-06 22:10:15 -0300

    esconde botão de teste do menu

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -62,7 +62,7 @@ export default function Index() {
           <SquareButton name="Cadastro Locador" variant="secondary" onPress={() => router.push("/signInLandLord")}/>
           <SquareButton name="Cadastro Locatário" variant="secondary" onPress={() => router.push("/signInRenter")}/>
           <SquareButton name="Login" onPress={() => router.push("/login")}/>
-          <SquareButton name="Teste componentes" onPress={() => router.push("/pvuLandLord")} />
+          {/*<SquareButton name="Teste componentes" onPress={() => router.push("/pvuLandLord")} />*/}
         </View>
       </View>
     </View>
