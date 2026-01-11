5c5071980329d02f9a19e9d0bfbbe5663858cf99
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2026-01-11 18:58:43 -0300

    fix supabase lock warning

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -10,7 +10,6 @@ export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
-    lock: processLock,
   },
 })
 
