2121ba5c7add370eba2e2dd9c40ef92f2759d84d
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2026-02-03 16:48:54 -0300

    Revert "Faz configs para testes e cria teste unitário 1"
    This reverts commit 2d5504e35370ec8e4ce712511abdb6fc7dab426e.

--- src/components/postBlock/styles.ts
+++ src/components/postBlock/styles.ts
@@ -1,5 +1,5 @@
-import { colors } from "../../styles/colors";
-import { fontSize } from "../../styles/fontSize";
+import { colors } from "@/styles/colors";
+import { fontSize } from "@/styles/fontSize";
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
