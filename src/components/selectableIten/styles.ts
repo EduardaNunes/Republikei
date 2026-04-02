00ed75358268da0053d3e592026d2301e5976446
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2026-04-02 12:53:38 -0300

    change ass property 3 page branding colors style

--- src/components/selectableIten/styles.ts
+++ src/components/selectableIten/styles.ts
@@ -1,12 +1,18 @@
 import { StyleSheet } from "react-native";
 import { colors } from "@/styles/colors";
+import { fontSize } from "@/styles/fontSize";
 
 export const styles = StyleSheet.create({
   container: {
     paddingHorizontal: 12,
     paddingVertical: 2,
-    borderColor: colors.orange[300],
+    borderColor: colors.backgroundGreen,
     borderWidth: 1,
     borderRadius: 16,
   },
+
+  text:{
+    color: colors.darkGray, 
+    fontSize: fontSize.text.small, 
+  }
 });
